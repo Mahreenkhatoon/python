a44edb7c00b594c0df531c36c1e0b6c3612d444e
Mahreen khatoon <mahreenkhatoon64@gmail.com>
2026-09-05 13:46:52 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+print("CALCULATOR")
+a=int(input("Enter the first number: "))
+op=input("Enter the operator: ")
+b=int(input("Enter the second number: "))
+if op=="+":
+    print(a+b)
+elif op=="-":
+    print(a-b)
+elif op=="*":
+    print(a*b)
+elif op=="/":
+    print(a/b)
